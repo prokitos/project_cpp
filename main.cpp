fff8de544355049e90385d56078cd4a20f2a38a3
prokitos <typada@yandex.ru>
2023-07-14 14:28:04 +0300

    try_catch

--- main.cpp
+++ main.cpp
@@ -23,8 +23,18 @@ int main()
 
     debora deb;
     deb.say();
-    deb.say();
     
+    
+    try
+    {
+        throw std::runtime_error("Division by zero!");
+    }
+    catch(const std::exception& e)
+    {
+        cout << "erroe = " << e.what();
+    }
+
+    cout << "aaaaa";
 
     
 

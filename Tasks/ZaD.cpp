d023c2ac2fc11af9d548778c9224213539e42593
prokitos <typada@yandex.ru>
2023-07-31 14:32:33 +0300

    codewar ku4 task

--- Tasks/ZaD.cpp
+++ Tasks/ZaD.cpp
@@ -1,4 +1,4 @@
-#include "zaC.h"
+#include "zaD.h"
 #include <vector>
 #include <algorithm>
 #include <math.h>
@@ -96,6 +96,108 @@ std::string format_time_new(int seconds)
 
 }
 
+struct PeakData 
+{
+  std::vector<int> pos, peaks;
+};
+
+PeakData pick_peaks(const std::vector<int> &v) 
+{
+    PeakData result;
+
+    bool longPick = false;
+    int longPickCount = 0;
+    for (size_t i = 1; i < v.size() - 1; i++)
+    {
+            // normal pick
+            if(v[i] > v[i-1] && v[i] > v[i+1])
+            {
+                result.peaks.push_back(v[i]);
+                result.pos.push_back(i);
+            }
+
+            // long pick
+            if(longPick == true)
+            {
+                if(v[i] == v[i + 1])
+                    longPickCount ++;
+                if(v[i] > v[i + 1])
+                {
+                    longPick = false;
+                    result.peaks.push_back(v[i - longPickCount]);
+                    result.pos.push_back(i - longPickCount);
+                    longPickCount = 0;
+                }
+                if(v[i] < v[i + 1])
+                {
+                    longPick = false;
+                    longPickCount = 0;
+                }
+            }
+            if(v[i] > v[i - 1] && v[i] == v[i + 1])
+            {
+                longPick = true;
+                longPickCount ++;
+            }
+    }
+    
+  return result;
+};
+
+std::string Mix::mix(const std::string &s1, const std::string &s2)
+{
+    std::string count1[26] = {};
+    std::string count2[26] = {};
+    std::vector<std::string> result;
+
+    // добавление строк в массивы
+    for (size_t i = 0; i < s1.length(); i++)
+    {
+        int charCode = s1[i] - 'a';
+        if(charCode >= 0 && charCode < 26)
+            count1[charCode] += s1[i];
+    }
+
+    for (size_t i = 0; i < s2.length(); i++)
+    {
+        int charCode = s2[i] - 'a';
+        if(charCode >= 0 && charCode < 26)
+            count2[charCode] += s2[i];
+    }
+
+    // добавление всех строк в один вектор + форматирование
+    for (size_t i = 0; i < std::size(count1); i++)
+    {
+        if(count1[i].length() == count2[i].length() && count1[i].length() > 1)
+        {
+            result.push_back("=:" + count1[i]);
+        }
+        else if(count1[i].length() > 1 && count1[i].length() > count2[i].length())
+        {
+            result.push_back("1:" + count1[i]);
+        }
+        else if(count2[i].length() > 1)
+        {
+            result.push_back("2:" + count2[i]);
+        }
+    }
+    
+    std::sort(result.begin(), result.end());
+    std::sort(result.begin(), result.end(), [](std::string& s1, std::string& s2) {return s1.length() > s2.length();});
+
+    std::string output {};
+    for (size_t i = 0; i < result.size(); i++)
+    {
+        if(i == 0)
+            output += result[i];
+        else
+            output += "/" + result[i];
+    }
+    
+    return output;
+}
+
+
 void zaMainD()
 {
     // зд 1
@@ -103,5 +205,20 @@ void zaMainD()
     //std::string res = format_duration(120); 
     //res = format_time_new(100000);
     //std::cout << res;
+
+    // зд 2
+    //std::vector<int> input = {3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3};
+    //PeakData output = pick_peaks(input);
+    //for(auto i : output.peaks)
+    //    std::cout << i << "  ";
+    //std::cout << std::endl;
+    //for(auto i : output.pos)
+    //    std::cout << i << "  ";
+
+    // зд 3
+    Mix classess;
+    std::cout << classess.mix("Are they here", "yes, they are here");
+    
+
 
 }
--- Tasks/zaD.h
+++ Tasks/zaD.h
@@ -3,6 +3,14 @@
 #include <map>
 
 
+struct PeakData;
+PeakData pick_peaks(const std::vector<int> &v);
+
+class Mix
+{
+public:
+  static std::string mix(const std::string &s1, const std::string &s2);
+};
 
 
 std::string format_duration(int seconds);

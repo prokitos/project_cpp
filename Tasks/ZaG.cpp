f5aa5f9fae0042002d40db574d960b1d34cb110c
prokitos <typada@yandex.ru>
2023-09-09 15:32:05 +0300

    codewar last edit

--- Tasks/ZaG.cpp
+++ Tasks/ZaG.cpp
@@ -43,75 +43,62 @@ void zaMainG()
     //std::cout << first << "   " << second;
 
     // зд 6 убрать множественные пробелы, сделать большие буквы и добавить точки.
-    //std::string stroka = "David holds a kite.  the salaryman kept the wallet.you   sell a chair.";
-    //std::cout << correct_it(stroka);
+    std::string stroka = "David holds a kite.  the salaryman kept the wallet.you   sell a chair You want end this.  ";
+    std::cout << correct_it(stroka) << "!!";    // проверка на пробелы в конце строки
+
+
 };
 
 std::string correct_it(std::string str) 
 {
     std::string newString {};
 
-    if(str[0] != ' ')
-    newString += str[0];
+    // добавить точки перед большими буквами
+    std::regex vowels("([A-Z])");
+    str = std::regex_replace(str, vowels, ".$1");
+    // убрать пробелы перед точками
+    vowels = ("( +)\\.");
+    str = std::regex_replace(str, vowels, ".");
+    // убрать пробелы после точек
+    vowels = ("\\.( +)");
+    str = std::regex_replace(str, vowels, ".");
+    // убрать множественные точки
+    vowels = ("\\.+");
+    str = std::regex_replace(str, vowels, ".");
 
-    bool isBig = false;  // для toUpper
-    bool isUpperExist = false; // если уже была большая буква
-    for (size_t i = 1; i < str.length(); i++)
+    // добавить большие буквы после точек
+    for (size_t i = 0; i < str.length(); i++)
     {
-        // добавить точки перед заглавными буквам
-        if(str[i] == std::toupper(str[i]) && (str[i] - 'A' > 0))
-        {
-            // в первый раз при заходе проигнорится, значит уже первая буква была.
-            if(isUpperExist == true)
-                newString += ". ";
-            isUpperExist = true;
-        }
-
-        // добавить заглавные буквы после точек. потом если найдена любая буква то она большая.
-        if(isBig == true && (str[i] - 'A' > 0 || str[i] - 'a' > 0))
-        {
-            if(str[i - 1] != ' ')
-                 newString += ' ';
-
-            newString += std::toupper(str[i]);
-            isBig = false; 
-            continue;
-        }
-        // если данный символ точка, то переключатель на тру, и следующая буква большая.
         if(str[i] == '.')
-        {
-            isBig = true;
-        }
-
-        // убрать возможность добавлять пустоту первым символом
-        if(str[i] == ' ' && newString == "")
-            continue;
-
-        if(newString == "" && (str[i] - 'A' > 0 || str[i] - 'a' > 0))
-        {
-            newString += std::toupper(str[i]);
+        {   
+            newString += str[i];
+            newString += std::toupper(str[i + 1]);
+            i ++;
             continue;
         }
-
         newString += str[i];
     }
 
-    // убираем множественные пробелы, и пробелы перед точками
-    std::regex vowels(" \\.");
-    newString = std::regex_replace(newString, vowels, ".");
+    // убираем множественные пробелы,
     vowels = (" +");
     newString = std::regex_replace(newString, vowels, " ");
-    vowels = ("\\.+");
-    newString = std::regex_replace(newString, vowels, ".");
-    vowels = (" $");
-    newString = std::regex_replace(newString, vowels, "");
 
-    if(newString[newString.length() - 1] != '.')
-    newString += '.';
+    // Добавляем пробел перед точками
+    vowels = ("(\\.)");
+    newString = std::regex_replace(newString, vowels, "$1 ");
+
+    //убрать точку в начале строки
+    vowels = ("^.");
+    newString = std::regex_replace(newString, vowels, "");
+    //убрать пробел в начале строки
+    vowels = ("^ ");
+    newString = std::regex_replace(newString, vowels, "");
+    //убрать пробел в конце строки
+    vowels = ("..$");
+    newString = std::regex_replace(newString, vowels, "");
 
     return newString;
 }
-
 
 bool valid_braces(std::string braces)
 {
